a3f4d107874c3c0ed3185a3560d8003d4d19b95b
Dominik <dominik.such.96@gmail.com>
2023-02-22 18:54:31 +0100

    test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 app.use(cors());
+app.use(express.static("build"));
 
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
